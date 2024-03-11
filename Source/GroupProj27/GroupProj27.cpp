0cf29867b986214c8eb42a1561fb51a46ee73e7f
Shahil Saha <71847625+shahilsaha05uk@users.noreply.github.com>
2024-03-11 15:16:09 +0000

    init

---
+++ Source/GroupProj27/GroupProj27.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GroupProj27.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GroupProj27, "GroupProj27" );
