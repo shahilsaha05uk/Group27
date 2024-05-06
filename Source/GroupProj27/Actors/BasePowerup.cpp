6000d60387ddd647112ea9266ff52b8981318924
Shahil Saha <71847625+shahilsaha05uk@users.noreply.github.com>
2024-05-06 15:21:35 +0100

    making the powerups
    includes:
    
    high jumps
    increased pizza quality
    and Sprint

---
+++ Source/GroupProj27/Actors/BasePowerup.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasePowerup.h"
+
+#include "Components/SphereComponent.h"
+
+
+ABasePowerup::ABasePowerup()
+{
+	mCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Collider"));
+	mCollider->SetupAttachment(RootComponent);
+	mCollider->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnOverlapBegin);
+	mCollider->OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnOverlapEnd);
+}
+
+void ABasePowerup::OnOverlapBegin_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+void ABasePowerup::OnOverlapEnd_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int OtherBodyIndex)
+{
+	
+}
+
+void ABasePowerup::ActivatePowerup_Implementation()
+{
+	
+}
+
+void ABasePowerup::DeactivatePowerup_Implementation()
+{
+	
+}
--- Source/GroupProj27/ParkourPlayer.cpp
+++ Source/GroupProj27/ParkourPlayer.cpp
@@ -12,47 +12,17 @@
 // Sets default values
 AParkourPlayer::AParkourPlayer()
 {
-	mLedgeColliderComponent = CreateDefaultSubobject<UCapsuleComponent>("LedgeCollider");
-	mLedgeColliderComponent->SetupAttachment(RootComponent);
-
 	mCameraHandlerComponent = CreateDefaultSubobject<USpringArmComponent>("Camera Handler");
 	mCameraHandlerComponent->SetupAttachment(RootComponent);
 
 	mCameraComponent = CreateDefaultSubobject<UCameraComponent>("Camera");
 	mCameraComponent->SetupAttachment(mCameraHandlerComponent);
-
-}
-
-void AParkourPlayer::OnPlayerOverlapBegin_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-}
-
-void AParkourPlayer::OnPlayerOverlapEnd_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int OtherBodyIndex)
-{
 }
 
 void AParkourPlayer::BeginPlay()
 {
-	// Do the bindings
-	mLedgeColliderComponent->OnComponentBeginOverlap.AddDynamic(this, &AParkourPlayer::OnPlatformCollisionBegin);
-	mLedgeColliderComponent->OnComponentEndOverlap.AddDynamic(this, &AParkourPlayer::OnPlatformCollisionEnd);
-	GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnPlayerOverlapBegin);
-	GetCapsuleComponent()->OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnPlayerOverlapEnd);
-
 	mDefaultWalkSpeed = GetCharacterMovement()->MaxWalkSpeed;
 	Super::BeginPlay();
-}
-
-void AParkourPlayer::OnPlatformCollisionBegin_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                                                             UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-}
-
-void AParkourPlayer::OnPlatformCollisionEnd_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int OtherBodyIndex)
-{
 }
 
 #pragma region Player Input Implementation
@@ -96,7 +66,6 @@ void AParkourPlayer::Slide_Implementation(const FInputActionValue& Value)
 
 }
 
-
 #pragma endregion
 
 void AParkourPlayer::SetWalkSpeed_Implementation(float Value)
@@ -119,12 +88,13 @@ void AParkourPlayer::OnOrdersRequested_Implementation()
 	
 }
 
-void AParkourPlayer::Hang_Implementation(bool isLedgeDetected)
+UCharacterMovementComponent* AParkourPlayer::GetMovementComp_Implementation()
 {
-	
+	return GetCharacterMovement();
 }
 
-void AParkourPlayer::StopHamg_Implementation()
+ACharacter* AParkourPlayer::GetCharacter_Implementation()
 {
-	
+	return this;
 }
+
